77c686844e33335315c122f4cf9ce31ca80a6b62
Zafer YILMAZ <zyilmaz@borcelik.com>
2021-01-09 23:47:45 +0300

    Lesson 3Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myTodoApp';
+  user = 'Zafer';
+  items = [
+    { description: "Kahvaltı", action:"No" },
+    { description: "Sinema", action:"Yes" },
+    { description: "Spor", action:"No" },
+    { description: "Ders Çalış", action:"No" },
+
+  ];
 }
